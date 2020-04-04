02b4d99ce2904ee76640d04fefb75bdb4d565c83
sarthak-1998 <sarthak240498@gmail.com>
2020-04-05 03:09:07 +0530

    mail id added

--- main.js
+++ main.js
@@ -68,5 +68,59 @@ function cursorAnimation() {
 
 
 
+//   /* Get the text field */
+//   var copyText = document.getElementById("myInput");
+
+//   /* Select the text field */
+// console.log(copyText.textContent);
+ 
+// var emailtext = copyText.textContent;
+//   //copyText.setSelectionRange(0, 99999); /*For mobile devices*/
+
+//   /* Copy the text inside the text field */
+//   document.execCommand('copy');
+
+//   /* Alert the copied text */
 
 
+$(document).ready(function() {
+	
+	// Add class to mailto link
+	// Needed to separate the disabling of the default action AND copy email to clipboard
+	$('a[href^=mailto]').addClass('mailto-link');
+
+	var mailto = $('.mailto-link');
+	var messageCopy = 'Click to copy email address';
+	var messageSuccess = 'Email address copied';
+	
+	mailto.append('<span class="mailto-message"></span>');
+	$('.mailto-message').append(messageCopy);
+	
+	// Disable default action (opening your email client. yuk.)
+	$('a[href^=mailto]').click(function() {
+		return false; 
+	})
+	
+	// On click, get href and remove 'mailto:'
+	// Store email address in a variable.
+	mailto.click(function() {
+		var href = $(this).attr('href');
+		var email = href.replace('mailto:', '');
+		copyToClipboard(email);
+		$('.mailto-message').empty().append(messageSuccess);
+		setTimeout(function() {
+			$('.mailto-message').empty().append(messageCopy);}, 2000); 
+	});
+	
+});
+
+// Grabbed this from Stack Overflow.
+// Copies the email variable to clipboard
+function copyToClipboard(text) {
+    var dummy = document.createElement("input");
+    document.body.appendChild(dummy);
+    dummy.setAttribute('value', text);
+    dummy.select();
+    document.execCommand('copy');
+    document.body.removeChild(dummy);
+}
